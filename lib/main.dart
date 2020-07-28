b10dd468ba948339864b65229e862d05f7e64ac6
Aljaz <aljazful@gmail.com>
2020-07-28 21:58:07 +0200

    attempted timer implementation

--- lib/main.dart
+++ lib/main.dart
@@ -43,9 +43,26 @@ Future<List<Level>> loadJson() async {
 class _TimerHomePageState extends State<TimerHomePage> {
   int _level = 1;
   num _time = 0;
+  num _totalTime;
+  num _shuttles;
+  List<Level> _data;
+  Timer _shuttleTimer;
+  Timer _totalTimer;
 
   void _startTimer() {
-
+    loadJson().then((value) {
+      setState(() {
+        _time = value[_level - 1].timePerShuttle;
+        _totalTime = value[_level - 1].totalLevelTime;
+        _shuttles = value[_level - 1].shuttles;
+      });
+      _shuttleTimer = Timer.periodic(Duration(seconds: 1), (timer){
+        setState(() {
+          if (_time > 0) _time--;
+          else _shuttles--;
+        });
+      });
+    });
   }
 
   @override
@@ -61,11 +78,16 @@ class _TimerHomePageState extends State<TimerHomePage> {
                 color: Colors.black, fontWeight: FontWeight.bold, fontSize: 38),
           ),
           Text(
-            '$_time s',
-            style: TextStyle(fontSize: 44),
+            'Total time: $_totalTime s',
+            style: TextStyle(fontSize: 36),
           ),
-          MaterialButton(onPressed: () => _startTimer(),
-          child: Text('Start', style: TextStyle(fontSize: 25)),
+          Text(
+            'Current shuttle: $_time s',
+            style: TextStyle(fontSize: 34),
+          ),
+          MaterialButton(
+            onPressed: () => _startTimer(),
+            child: Text('Start', style: TextStyle(fontSize: 25)),
             color: Color.fromRGBO(19, 168, 58, .6),
           )
         ],
