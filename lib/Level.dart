e98c997615057a54ef9d462bbd7869e787c4b0ae
Aljaz <aljazful@gmail.com>
2020-07-28 17:57:50 +0200

    fixed pubspec, json loading

--- lib/Level.dart
+++ lib/Level.dart
@@ -1,16 +1,16 @@
 class Level {
-  String level;
-  String shuttles;
-  String runningSpeed;
-  String timePerShuttle;
-  String totalLevelTime;
-  String levelDistance;
+  num level;
+  num shuttles;
+  num runningSpeed;
+  num timePerShuttle;
+  num totalLevelTime;
+  num levelDistance;
 
   Level.fromJson(Map jsonMap)
   : level = jsonMap['level'],
     shuttles = jsonMap['shuttles'],
-    runningSpeed = jsonMap['running_speed'],
-    timePerShuttle = jsonMap['time_per_shuttle'],
-    totalLevelTime = jsonMap['total_level_time'],
+    runningSpeed = jsonMap['running_speed'] as num,
+    timePerShuttle = jsonMap['time_per_shuttle'] as num,
+    totalLevelTime = jsonMap['total_level_time'] as num,
     levelDistance = jsonMap['level_distance'];
 }
