6d83d8c1c87a7ed34c28879cbd97558d04474edd
Aljaz <aljazful@gmail.com>
2020-07-30 14:43:54 +0200

    fixed my dyslexia

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'components/TimerHomePageStatr.dart';
+import 'components/TimerHomePageStart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
